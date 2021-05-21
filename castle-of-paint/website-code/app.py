b7a2daaed5a700a484b32a1b21a523e88c6a301a
Ian Roberts <iar31.roberts@gmail.com>
2021-05-21 15:01:58 +0100

    add elments page

--- castle-of-paint/website-code/app.py
+++ castle-of-paint/website-code/app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('elements')
+def elements():
+    return render_template('elements.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
