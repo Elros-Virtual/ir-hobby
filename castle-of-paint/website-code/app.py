1231a0578c8673ac1bd61a55be6018b7a08e75d6
Ian Roberts <iar31.roberts@gmail.com>
2021-05-21 15:10:43 +0100

    remove elements page

--- castle-of-paint/website-code/app.py
+++ castle-of-paint/website-code/app.py
@@ -9,9 +9,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('elements')
-def elements():
-    return render_template('elements.html')
+# @app.route('elements')
+# def elements():
+#     return render_template('elements.html')
 
 
 if __name__ == '__main__':
