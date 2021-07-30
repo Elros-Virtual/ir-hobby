6ae21073e5003bab91b36e6ec132a4a92b9f602d
Ian Roberts <iar31.roberts@gmail.com>
2021-07-30 16:03:13 +0200

    add new website details

---
+++ castle-of-paint/website-code-new/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/canvaspainting')
+def canvaspainting():
+    return render_template('canvaspainting.html')
+
+
+@app.route('/miniturepainting')
+def miniturepainting():
+    return render_template('miniturepainting.html')
+
+
+@app.route('/digitalart')
+def digitalart():
+    return render_template('digitalart.html')
+
+
+@app.route('/pencilsketching')
+def pencilsketching():
+    return render_template('pencilsketching.html')
+
+
+if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
